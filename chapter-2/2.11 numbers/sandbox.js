3e085345f69223de2dd8166e018fc3556dc832c8
Evgeny Khomutenko <ekhomutenko@ptsecurity.com>
2020-04-05 20:12:21 +0700

    lesson-11

---
+++ chapter-2/2.11 numbers/sandbox.js
@@ -0,0 +1,29 @@
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+
+// math operators +, -, *, /, (** - степень),(% - остаток от деления)
+ 
+console.log(10/2);
+console.log(radius / 2);
+
+let result = radius % 3;
+console.log(result);
+
+let result2 = pi * radius**2;
+console.log(result2);
+
+let likes = 10;
+likes ++; //прибавить 1 и записать в переменную
+likes --;
+likes += 10; //прибавить 10 и записать в переменную
+likes *= 2;
+console.log(likes);
+
+//NaN - not a number
+console.log(5/'hello');
+
+//Вставить число в строку
+let result3 = 'the blog has' + ' ' + likes + ' ' + 'likes';
+console.log(result3);
