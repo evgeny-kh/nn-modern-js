d402e64c355c51b42646c75f9e5b0ded1d8fb7c3
Evgeny Khomutenko <ekhomutenko@ptsecurity.com>
2020-04-05 19:50:26 +0700

    lesson-9

---
+++ lesson-9/sandbox.js
@@ -0,0 +1,27 @@
+// string
+console.log('hello,world');
+
+let email = 'my-email@email.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length)
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+// найти положение символа в строке
+let index = email.indexOf('@');
+console.log(index);
