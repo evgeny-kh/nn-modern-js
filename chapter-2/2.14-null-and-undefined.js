1b15f9ab343ffba55c8cbb027d85e1f72da9c8dc
Evgeny Khomutenko <ekhomutenko@ptsecurity.com>
2020-04-25 18:53:37 +0700

    2.15-boolean-and-comparison

--- chapter-2/2.14-null-and-undefined.js
+++ chapter-2/2.14-null-and-undefined.js
@@ -1,2 +1,5 @@
+//null is useful for cleaning strings
 let age = null;
 console.log(age, age + 3, `the age is ${age}`); 
+
+//undefined shows up when a value is not defined at all;
---
+++ chapter-2/2.15-boolean-and-comparisons.js
@@ -0,0 +1,25 @@
+//look for an argument in the variable
+let email = 'email@email.com';
+let result = email.includes('@');
+console.log(result);
+
+//look for an argument in the array as an element
+let names = ['john','peter'];
+let result2 = names.includes('peter');
+console.log(result2);
+
+//comparison operators
+let age = 25;
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age < 30);
+console.log(age > 30);
+console.log(age >= 25);
+console.log(age >= 25);
+
+let name = 'shaun';
+console.log(name == 'shaun');
+console.log(name == 'Shaun'); //false
+console.log(name > 'crystal'); //true cas 's' stand latter in the alphabet
+console.log(name > 'Shaun'); //false
