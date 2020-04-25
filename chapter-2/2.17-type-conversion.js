d0d2c971cf4a2d510f60eb7cb0ba305ff13e19a6
Evgeny Khomutenko <ekhomutenko@ptsecurity.com>
2020-04-25 19:46:14 +0700

    2.17-type-conversion

---
+++ chapter-2/2.17-type-conversion.js
@@ -0,0 +1,21 @@
+let score = '100';
+console.log(score + 1); //1001
+
+// override variable with a numbered value of it
+
+score = Number(score);
+console.log(score +1); //101
+console.log(typeof score); //number
+
+score = String(score);
+console.log(score + 1); //1001
+console.log(typeof score); //string
+
+
+// you can't convert string to number if it consist of letters
+let result = Number('score');
+console.log(result); //NaN (not a number)
+
+// positive numbers are considered as boolean true values
+let bool = Boolean(100);
+console.log(bool);
