136c9d7be12bbbdcd1414b25b4a6f00d83a3bc64
Evgeny Khomutenko <ekhomutenko@ptsecurity.com>
2020-04-25 18:32:42 +0700

    2.13-arrays

---
+++ chapter-2/2.13-arrays.js
@@ -0,0 +1,38 @@
+//Declare the array
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+//Pick override array element
+ninjas[1] = 'ken';
+
+console.log(ninjas[1]);
+
+let ages = [20,25,30,35];
+console.log(ages[2]);
+
+//Multi-type array
+let random = ['shaun', 'crystal', 30, 30];
+
+//Write the array length
+console.log(ninjas.length);
+
+//Array methods
+
+//Connect elements into one string separated with the argument
+let result = ninjas.join('-');
+console.log(result);
+
+//Write element's position
+let result2 = ninjas.indexOf('shaun');
+console.log(result2);
+
+//Concat two arrays
+let result3 = ninjas.concat(['ken', 'crystal']);
+console.log(result3);
+
+//Add a new value in array
+let result4 = ninjas.push('joe');
+console.log(ninjas);
+
+//Show the last value
+result4 = ninjas.pop();
+console.log(result4);
