416be91f963ed9d3a2d51d133c1b387f85c995b1
Evgeni Homutenko <evgeni.homutenko@gmail.com>
2020-07-08 17:56:51 +0300

    parents, children and siblings

---
+++ chapter-6/52-parents-children-and-siblings/script.js
@@ -0,0 +1,11 @@
+const article = document.querySelector('article');
+console.log(article.children);
+
+// putting a collection into an array
+Array.from(article.children).forEach(child => {
+    child.classList.add('article-element');
+})
+
+const title = document.querySelector('h1');
+console.log(title.parentElement);
+console.log(title.parentElement.parentElement);
